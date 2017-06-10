48d10d437006b8dc9d03a1d154bcbd5dfa97053f
jobtalle <jobtalle@hotmail.com>
2017-06-11 00:26:55 +0200

    Implemented additional vector operations

--- myriad/types/vector.cpp
+++ myriad/types/vector.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #include "vector.h"
 
 myr::Vector::Vector(const float x = 0, const float y = 0)
@@ -8,3 +10,53 @@ myr::Vector::Vector(const int x, const int y)
 
 myr::Vector::Vector(const unsigned int x, const unsigned int y)
 :x(float(x)), y(float(y)) {}
+
+float myr::Vector::dot(const Vector &other)
+{
+	return x * other.x + y * other.y;
+}
+
+float myr::Vector::length()
+{
+	return sqrt(dot(*this));
+}
+
+myr::Vector myr::Vector::normalized()
+{
+	const float scale = 1.0f / length();
+
+	return Vector(
+		x * scale,
+		y * scale);
+}
+
+myr::Vector &myr::Vector::normalize()
+{
+	const float scale = 1.0f / length();
+
+	x *= scale;
+	y *= scale;
+
+	return *this;
+}
+
+myr::Vector myr::Vector::operator+(const Vector &other)
+{
+	return Vector(
+		x + other.x,
+		y + other.y);
+}
+
+myr::Vector myr::Vector::operator-(const Vector &other)
+{
+	return Vector(
+		x - other.x,
+		y - other.y);
+}
+
+myr::Vector myr::Vector::operator*(const Vector &other)
+{
+	return Vector(
+		x * other.x,
+		y * other.y);
+}
--- myriad/types/vector.h
+++ myriad/types/vector.h
@@ -11,5 +11,14 @@ namespace myr
 		Vector(const float x, const float y);
 		Vector(const int x, const int y);
 		Vector(const unsigned int x, const unsigned int y);
+
+		float dot(const Vector &other);
+		float length();
+		Vector normalized();
+		Vector &normalize();
+
+		Vector operator+(const Vector &other);
+		Vector operator-(const Vector &other);
+		Vector operator*(const Vector &other);
 	};
 }
